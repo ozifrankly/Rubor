2a48342656d297dbd0f70745ad395b037247626d
Yan Justino <yanlimaj@gmail.com>
2014-08-16 21:11:23 -0300

    Initial Commit

---
+++ Rubor/Object.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubor
+{
+  public static class Object
+  {
+    public delegate void Block();
+  }
+}
