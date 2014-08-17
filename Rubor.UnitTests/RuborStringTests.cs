f061d60bed74b55775ad74ee400d4490c5409fad
Yan Justino <yanlimaj@gmail.com>
2014-08-17 20:00:10 -0300

    Add string features

---
+++ Rubor.UnitTests/RuborStringTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Rubor.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rubor.UnitTests
+{
+  [TestClass]
+  public class RuborStringTests
+  {
+    [TestMethod]
+    public void CapitalizeTest()
+    {
+      var test = "test";
+
+      Assert.AreEqual("test", test);
+      Assert.AreEqual("Test", test.Capitalize());
+    }
+
+    [TestMethod]
+    public void ClearTest()
+    {
+      var test = "test";
+
+      Assert.AreEqual("test", test);
+      Assert.AreEqual("", test.Clear());
+    }
+
+    [TestMethod]
+    public void DownCaseTest()
+    {
+      var test = "TEST";
+
+      Assert.AreEqual("TEST", test);
+      Assert.AreEqual("test", test.DownCase());
+    }
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Assert.IsTrue("".Empty());
+      Assert.IsFalse("test".Empty());
+    }
+
+    [TestMethod]
+    public void UpCaseTest()
+    {
+      Assert.AreEqual("TEST","test".UpCase());
+    }
+  }
+}
---
+++ Rubor/Core/RuborString.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rubor.Core
+{
+  public static class RuborString
+  {
+    /// <summary>
+    /// Returns a copy of str with the first character 
+    /// converted to uppercase and the remainder to lowercase. 
+    /// Note: case conversion is effective only in ASCII region.
+    /// </summary>
+    public static string Capitalize(this string value)
+    {
+      return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value);
+    }
+
+    /// <summary>
+    /// Makes string empty.
+    /// </summary>
+    public static string Clear(this string value)
+    {
+      return string.Empty;
+    }
+
+    /// <summary>
+    /// Returns a copy of str with all uppercase letters replaced with 
+    /// their lowercase counterparts. The operation is locale 
+    /// insensitive—only characters “A” to “Z” are affected. 
+    /// Note: case replacement is effective only in ASCII region.
+    /// </summary>
+    public static string DownCase(this string value)
+    {
+      return value.ToLower();
+    }
+
+    /// <summary>
+    /// Returns true if str has a length of zero.
+    /// </summary>
+    public static bool Empty(this string value)
+    {
+      return string.IsNullOrEmpty(value);
+    }
+
+    /// <summary>
+    /// pcases the contents of str, returning nil 
+    /// if no changes were made. Note: case replacement 
+    /// is effective only in ASCII region.
+    /// </summary>
+    public static string UpCase(this string value)
+    {
+      return value.ToUpper();
+    }
+  }
+}
